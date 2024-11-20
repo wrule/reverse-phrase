1b8939f0a2b48a351e7a571df4bc91f7a03436e5
jimao <shihao.gu@perfma.com>
2024-11-20 13:36:50 +0800

    page

--- apps/ui/src/app/hook/page.tsx
+++ apps/ui/src/app/hook/page.tsx
@@ -1,6 +1,26 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 
 const HookPage = () => {
-  return <div>Hook</div>;
+  const nowTimeStr = () => new Date().toLocaleString();
+
+  const [timeNow, setTimeNow] = useState<string>(nowTimeStr());
+  const updateTimeNow = () => {
+    setTimeNow(nowTimeStr());
+  };
+
+  useEffect(() => {
+    updateTimeNow();
+    const timer = setInterval(() => {
+      updateTimeNow();
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return <div>Hook: {timeNow}</div>;
 };
 
 export default HookPage;

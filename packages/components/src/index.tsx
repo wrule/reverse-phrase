a30f0ea75a13b2daaa46fbdb5d46dd77c75b7374
jimao <shihao.gu@perfma.com>
2024-11-21 23:16:33 +0800

    TimeDisplay导出方式

--- packages/components/src/index.tsx
+++ packages/components/src/index.tsx
@@ -1,9 +1,8 @@
 'use client';
 import { useTimeNow } from '@rp/hooks';
 
+export
 const TimeDisplay = ({ initialTime }: { initialTime: string }) => {
   const time = useTimeNow(initialTime);
   return <div>Time: {time}</div>;
 };
-
-export default TimeDisplay;
